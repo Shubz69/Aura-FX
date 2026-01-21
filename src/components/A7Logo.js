7c243a2616036efe3626e5d483d2f6ce565dcb30
Shubz69 <Shubzfx@gmail.com>
2026-01-21 17:20:07 +0000

    Redesign A7 logo to match image - A with apex and high crossbar, 7 integrated with horizontal top bar aligned with crossbar and diagonal down-left, all white

--- src/components/A7Logo.js
+++ src/components/A7Logo.js
@@ -5,13 +5,15 @@ const A7Logo = () => {
     return (
         <div className="a7-logo-container">
             <div className="a7-logo-symbol">
-                {/* A shape - left diagonal (meets at bottom, open at top) */}
+                {/* A shape - left diagonal (from bottom-left to apex) */}
                 <div className="a7-diagonal a7-diagonal-left"></div>
-                {/* A shape - right diagonal (meets at bottom, open at top) */}
+                {/* A shape - right diagonal (from apex to bottom-center) - also serves as left side of 7 */}
                 <div className="a7-diagonal a7-diagonal-right"></div>
-                {/* 7 shape - horizontal bar at top (to the right of A) */}
+                {/* A shape - short horizontal crossbar (high up, connects the two diagonals) */}
                 <div className="a7-horizontal a7-crossbar"></div>
-                {/* 7 shape - diagonal stroke (down-left from horizontal bar, connecting to A) */}
+                {/* 7 shape - horizontal top bar (extends right from crossbar, aligns with it) */}
+                <div className="a7-horizontal a7-topbar"></div>
+                {/* 7 shape - diagonal stroke (down and slightly left from right end of top bar) */}
                 <div className="a7-diagonal a7-diagonal-seven"></div>
             </div>
         </div>
