d3450a5fc5e64c16eda8300c043c3dd1b33ae840
Shubz69 <Shubzfx@gmail.com>
2026-01-21 17:43:50 +0000

    Add AURA FX text back to navbar and increase A7 logo size to match text

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -39,8 +39,9 @@ const Navbar = () => {
         <nav className="navbar">
             <div className="logo-container">
                 <Link to="/" className="logo-link">
-                    <div className="navbar-logo-wrapper">
+                    <div className="navbar-logo-wrapper" style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', gap: '10px' }}>
                         <A7Logo />
+                        <span className="logo">AURA FX</span>
                     </div>
                 </Link>
             </div>
