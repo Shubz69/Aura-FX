dd18396aa45f9efcf02b42457fe7103c4b80b4c7
Shubz69 <Shubzfx@gmail.com>
2026-01-21 17:13:51 +0000

    Fix A7 logo design - A meets at bottom (open top), 7 has horizontal bar + diagonal down-left, all white, proper sizing for navbar and home page

--- src/components/A7Logo.js
+++ src/components/A7Logo.js
@@ -5,13 +5,13 @@ const A7Logo = () => {
     return (
         <div className="a7-logo-container">
             <div className="a7-logo-symbol">
-                {/* A shape - left diagonal */}
+                {/* A shape - left diagonal (meets at bottom, open at top) */}
                 <div className="a7-diagonal a7-diagonal-left"></div>
-                {/* A shape - right diagonal */}
+                {/* A shape - right diagonal (meets at bottom, open at top) */}
                 <div className="a7-diagonal a7-diagonal-right"></div>
-                {/* A shape - horizontal crossbar (shared with 7) */}
+                {/* 7 shape - horizontal bar at top (to the right of A) */}
                 <div className="a7-horizontal a7-crossbar"></div>
-                {/* 7 shape - diagonal stroke (down-right) */}
+                {/* 7 shape - diagonal stroke (down-left from horizontal bar, connecting to A) */}
                 <div className="a7-diagonal a7-diagonal-seven"></div>
             </div>
         </div>
