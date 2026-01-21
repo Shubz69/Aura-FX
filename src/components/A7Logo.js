1c97318ada90c9758f8ad32fb9151c2cc980669d
Shubz69 <Shubzfx@gmail.com>
2026-01-21 17:37:27 +0000

    Replace CSS-based A7 logo with image file from public/logos/a7-logo.png

--- src/components/A7Logo.js
+++ src/components/A7Logo.js
@@ -4,18 +4,11 @@ import '../styles/A7Logo.css';
 const A7Logo = () => {
     return (
         <div className="a7-logo-container">
-            <div className="a7-logo-symbol">
-                {/* Flat top - Continuous horizontal bar spanning both A and 7 */}
-                <div className="a7-horizontal a7-flat-top"></div>
-                {/* A shape - left diagonal (from bottom-left up to flat top) */}
-                <div className="a7-diagonal a7-diagonal-left"></div>
-                {/* A shape - right diagonal (from bottom-center up to flat top) */}
-                <div className="a7-diagonal a7-diagonal-right"></div>
-                {/* A shape - horizontal crossbar (2/3 down from flat top, connects the two diagonals) */}
-                <div className="a7-horizontal a7-crossbar"></div>
-                {/* 7 shape - diagonal stroke (from right end of flat top, down-left, parallel to A's right diagonal) */}
-                <div className="a7-diagonal a7-diagonal-seven"></div>
-            </div>
+            <img 
+                src="/logos/a7-logo.png" 
+                alt="A7 Logo" 
+                className="a7-logo-image"
+            />
         </div>
     );
 };
